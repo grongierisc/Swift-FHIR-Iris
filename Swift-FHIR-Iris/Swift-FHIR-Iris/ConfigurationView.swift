086a44437295942baa52198b47dbaac4cfe78df7
grongierisc <guillaume.rongier@intersystems.com>
2020-10-10 17:55:02 +0200

    Init commit

---
+++ Swift-FHIR-Iris/Swift-FHIR-Iris/ConfigurationView.swift
@@ -0,0 +1,20 @@
+//
+//  ConfigurationView.swift
+//  Swift-FHIR-Iris
+//
+//  Created by Guillaume Rongier on 09/10/2020.
+//
+
+import SwiftUI
+
+struct ConfigurationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConfigurationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfigurationView()
+    }
+}
---
+++ Swift-FHIR-Iris/Swift-FHIR-Iris/Swift_FHIR_IrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_FHIR_IrisApp.swift
+//  Swift-FHIR-Iris
+//
+//  Created by Guillaume Rongier on 09/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Swift_FHIR_IrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
